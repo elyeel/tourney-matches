33c2aebe27d260a66acf0c6339f9b47b4a5142be
Risyardi Iswanto <elyeel@yahoo.com>
2021-08-24 13:21:12 -0700

    refactored app, moved playerHelper to app

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import playerData from './components/data/playerData';
 import matchData from './components/data/matchData';
 import MatchList from './components/matchList/MatchList';
 import PlayerList from './components/playerList/PlayerList';
+import {
+	preparePlayerData,
+	addWinsToPlayers
+} from './components/helpers/playerHelpers';
 import './App.css';
 
 export default function App() {
+	const playerDataArray = preparePlayerData(playerData);
+	const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
+
 	return (
 		<div className="App">
 			<h1>
 				Tourney Matches{' '}
 				<span>Where Coding and Tournaments found their Match!</span>
 			</h1>
-			<PlayerList matchData={matchData} playerData={playerData} />
+			<PlayerList parsedPlayerData={parsedPlayerData} />
 			<MatchList matchData={matchData} />
 		</div>
 	);
--- src/components/playerList/PlayerList.js
+++ src/components/playerList/PlayerList.js
@@ -1,11 +1,9 @@
 import Player from '../player/Player';
 // import playerData from '../data/playerData';
-import { preparePlayerData, addWinsToPlayers } from '../helpers/playerHelpers';
+// import { preparePlayerData, addWinsToPlayers } from '../helpers/playerHelpers';
 // import matchData from '../data/matchData';
 
-export default function PlayerList({ playerData, matchData }) {
-	const playerDataArray = preparePlayerData(playerData);
-	const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
+export default function PlayerList({ parsedPlayerData }) {
 	const onePlayer = parsedPlayerData[0];
 	// console.log(onePlayer);
 
