b422610df02a8de774aa89dba8142fdb4db12afd
Risyardi Iswanto <elyeel@yahoo.com>
2021-08-24 13:46:57 -0700

    mapped matches and players by refactoring playerList

--- src/components/matchList/MatchList.js
+++ src/components/matchList/MatchList.js
@@ -2,14 +2,16 @@ import Match from '../match/Match';
 // import matchData from '../data/matchData';
 
 export default function MatchList({ matchData }) {
-	const oneMatch = matchData[0];
+	const parsedMatches = matchData.map((match) => (
+		<Match key={match.matchNumber} {...match} />
+	));
 	// console.log(oneMatch);
 
 	return (
 		<section className="PlayerList MatchList">
 			<h1>Match list</h1>
 			{/* Matches will be shown here */}
-			<Match {...oneMatch} />
+			{parsedMatches}
 		</section>
 	);
 }
--- src/components/playerList/PlayerList.js
+++ src/components/playerList/PlayerList.js
@@ -4,14 +4,17 @@ import Player from '../player/Player';
 // import matchData from '../data/matchData';
 
 export default function PlayerList({ parsedPlayerData }) {
-	const onePlayer = parsedPlayerData[0];
+	const parsedPlayers = parsedPlayerData.map((player) => (
+		<Player key={player.gamerTag} {...player} />
+	));
+	// const onePlayer = parsedPlayerData[0];
 	// console.log(onePlayer);
 
 	return (
 		<section className="PlayerList">
 			<h1>Current participating players</h1>
-			<Player {...onePlayer} />
 			{/* Players will be shown here */}
+			{parsedPlayers}
 		</section>
 	);
 }
